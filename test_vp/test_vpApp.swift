e4facbe287fa7836a884cfe4c8e547b0fc73c46b
brycedli <bryce.d.li@gmail.com>
2024-01-24 11:35:45 -0500

    Initial Commit

---
+++ test_vp/test_vpApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_vpApp.swift
+//  test_vp
+//
+//  Created by Bryce Li on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct test_vpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
