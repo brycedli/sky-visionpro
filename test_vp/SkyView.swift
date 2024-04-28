c8d5e410d2e8852e0902e172814e1faf404c892c
brycedli <bryce.d.li@gmail.com>
2024-04-27 20:50:00 -0400

    video

--- test_vp/SkyView.swift
+++ test_vp/SkyView.swift
@@ -28,12 +28,12 @@ struct SkyView: View {
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) { // Adjust delay as needed
             let r2 = Transform(pitch: .pi/2 * 1, yaw: .pi, roll: 0)
-
+            
             let to1 = Transform(scale: SIMD3(repeating: 0.05), rotation: r2.rotation, translation: SIMD3(0, 1.5, -1.5))
             
             portal.move(to: to1, relativeTo: nil, duration: 1, timingFunction: .easeIn)
             DispatchQueue.main.asyncAfter(deadline: .now() + 1) { // Adjust delay as needed
-
+                
                 let to2 = Transform(scale: SIMD3(repeating: size), rotation: r2.rotation, translation: SIMD3(0, 2.5, -1.5))
                 portal.move(to: to2, relativeTo: nil, duration: 4, timingFunction: .easeOut)
             }
@@ -57,26 +57,28 @@ struct SkyView: View {
             let world = Entity()
             world.components[WorldComponent.self] = .init()
             
-            let url = Bundle.main.url(forResource: "sky", withExtension: "MOV")!
+            let url = Bundle.main.url(forResource: "cloud_0_5", withExtension: "mp4")!
             let asset = AVURLAsset(url: url)
             let playerItem = AVPlayerItem(asset: asset)
             let player = AVPlayer()
             
             let skyVideoMaterial = VideoMaterial(avPlayer: player)
-            
+//            let skyRect = Entity()
+//            skyRect.components.set(ModelComponent(mesh: .generatePlane(width: 1E3, depth: 1E3), materials: [skyVideoMaterial]))
+//            skyRect.transform.translation = .init(x: 0, y: 1E3, z: -1E3)
+//            world.addChild(skyRect)
             let skySphere = Entity()
             skySphere.components.set(ModelComponent(mesh: .generateSphere(radius: 1E3), materials: [skyVideoMaterial]))
             skySphere.scale = .init(x: 1, y: 1, z: -1)
-            let rotationAngle = Float(Angle(degrees: -130).radians)
-            
-            skySphere.transform.rotation = .init(angle: rotationAngle, axis: .init(1.0, 0.0, 0.0))
-            
+            let rotationAngle = Float(Angle(degrees: -90).radians)
+            skySphere.transform.rotation = .init(angle: rotationAngle, axis: .init(0.0, 1.0, 0.0))
+            let rotationAngle2 = Float(Angle(degrees: -45).radians)
+            skySphere.transform.rotation *= .init(angle: rotationAngle2, axis: .init(0.0, 0.0, 1.0))
             world.addChild(skySphere)
             
             
             player.replaceCurrentItem(with: playerItem)
             player.play()
-            
             NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: player.currentItem, queue: nil) { notif in
                 player.seek(to: .zero)
                 player.play()
@@ -85,9 +87,7 @@ struct SkyView: View {
             portal.components[ModelComponent.self] = .init(mesh: .generatePlane(width: 1.5, height: 1.5, cornerRadius: 1), materials: [PortalMaterial()])
             portal.components[PortalComponent.self] = .init(target: world)
             portal.transform = Transform(pitch: .pi/2, yaw: 0, roll: 0)
-//            portal.position.y += 2.5
-//            portal.position.z -= 1.5
-            portal.position = SIMD3(0, 2.5, -1.5)
+            portal.position = SIMD3(0, 2.5, -2)
             portal.setScale(SIMD3(repeating: 0.001), relativeTo: nil)
             content.add(world)
             basePortals.addChild(portal)
@@ -100,11 +100,12 @@ struct SkyView: View {
             
             rainbow.position.z -= 0.01
             
-            let textMesh = textGen(textString: "hello world")
-            world.addChild(textMesh)
-            textMesh.position = portal.position + 150 * SIMD3(0, 1, -1)
-            textMesh.scale = SIMD3(repeating: 200)
-            textMesh.look(at: SIMD3.zero, from: textMesh.position, relativeTo: nil)
+//            let textMesh = textGen(textString: "hello world")
+//            world.addChild(textMesh)
+////            content.add(textMesh)
+//            textMesh.position = portal.position + 150 * SIMD3(0, 1, -1.5)
+//            textMesh.scale = SIMD3(repeating: 200)
+//            textMesh.look(at: SIMD3.zero, from: textMesh.position, relativeTo: nil)
             Task {
                 expand (size: 1.3)
             }
@@ -112,37 +113,41 @@ struct SkyView: View {
     }
     
     func textGen(textString: String) -> Entity {
-          
-          let materialVar = SimpleMaterial(color: .white, roughness: 0, isMetallic: false)
-          
-          let depthVar: Float = 0.001
-//          let fontVar = UIFont.systemFont(ofSize: 0.1)
+        
+        let depthVar: Float = 0.001
+        //          let fontVar = UIFont.systemFont(ofSize: 0.1)
         let fontVar = UIFont(name: "CirrusCumulus", size: 0.1)!
         let containerFrameVar = CGRect.zero
-          let alignmentVar: CTTextAlignment = .center
-          let lineBreakModeVar : CTLineBreakMode = .byWordWrapping
-          
-          let textMeshResource : MeshResource = .generateText(textString,
-                                             extrusionDepth: depthVar,
-                                             font: fontVar,
-                                             containerFrame: containerFrameVar,
-                                             alignment: alignmentVar,
-                                             lineBreakMode: lineBreakModeVar)
-          let textEntity = Entity()
+        let alignmentVar: CTTextAlignment = .center
+        let lineBreakModeVar : CTLineBreakMode = .byWordWrapping
+        
+        let textMeshResource : MeshResource = .generateText(textString,
+                                                            extrusionDepth: depthVar,
+                                                            font: fontVar,
+                                                            containerFrame: containerFrameVar,
+                                                            alignment: alignmentVar,
+                                                            lineBreakMode: lineBreakModeVar)
+        let textEntity = Entity()
         let textChildren = Entity()
         textEntity.addChild(textChildren)
-//          let textEntity = ModelEntity(mesh: textMeshResource, materials: [materialVar])
+        //          let textEntity = ModelEntity(mesh: textMeshResource, materials: [materialVar])
         var positions: Array<SIMD3<Float>> = Array()
-        let cubeMeshResource : MeshResource = .generatePlane(width: 0.020, height: 0.020)
-//        let cubeMeshResource : MeshResource = .generateSphere(radius: 0.04)(width:
-
+        let cubeMeshResource : MeshResource = .generatePlane(width: 0.060, height: 0.060)
+        //        let cubeMeshResource : MeshResource = .generateSphere(radius: 0.04)(width:
+        
         debugPrint(textMeshResource.contents.models.count)
         var pointAverage:SIMD3<Float> = SIMD3.zero
+        
+        var material = PhysicallyBasedMaterial()
+        material.baseColor = try! .init(tint: UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1), texture: .init(.load(named: "whitecloud.png", in: nil)))
+        material.opacityThreshold = 0.0 // IMPORTANT
+        
+        material.faceCulling = .none
+        material.emissiveColor = try! .init(color: UIColor.white, texture: .init(.load(named: "whitecloud.png", in: nil)))
+        material.emissiveIntensity = 10
         textMeshResource.contents.models.forEach {model in
             model.parts.forEach {part in
                 positions = Array(part.positions)
-//                positions = stride(from: 0, to: positions.count, by: 5).map { positions[$0] }
-                
                 var buckets = [SIMD3<Int>: [SIMD3<Float>]]()
                 let gridSize: Float = 0.01 // Change this based on your spatial density requirements
                 
@@ -159,7 +164,7 @@ struct SkyView: View {
                 var thinnedPoints: [SIMD3<Float>] = []
                 
                 
-                for (key, bucketPoints) in buckets {
+                for (_, bucketPoints) in buckets {
                     if bucketPoints.count > maxPointsPerBucket {
                         // Here we choose the first point, you could also choose a random point or calculate the centroid
                         thinnedPoints.append(bucketPoints[0])
@@ -168,52 +173,28 @@ struct SkyView: View {
                     }
                 }
                 
-
-//                var material = SimpleMaterial()
-//
-//                material.color = try! .init(tint: .white.withAlphaComponent(0.9999),
-//                                         texture: .init(.load(named: "cloud-lighter.png", in: nil)))
                 
-                var material = PhysicallyBasedMaterial()
-                material.baseColor = try! .init(texture: .init(.load(named: "cloud-lighter.png", in: nil)))
-//                material.blending = .init(blending: .transparent(opacity: 0.9999))
-                material.opacityThreshold = 0.0 // IMPORTANT
-                
-                material.faceCulling = .none
-//                material.emissiveColor.color = UIColor.white
-                material.emissiveIntensity = 1
                 
                 for point in thinnedPoints {
                     pointAverage = pointAverage + point
                     let cloud = ModelEntity(mesh: cubeMeshResource, materials: [material])
-//                    let cloud = rainbow.clone(recursive: true)
-                   
-//                    cloud.transform = Transform(pitch: .pi/2 * 1, yaw: .pi, roll: 0)
                     cloud.position = point
                     cloud.position = SIMD3(cloud.position.x, cloud.position.y, Float.random(in: 0.0 ..< 0.01))
                     cloud.transform.rotation = .init(angle: Float.random(in: 0.0 ..< .pi * 2), axis: .init(0.0, 0.0, 1.0))
-                    cloud.scale = SIMD3(repeating: Float.random(in: 0.5 ..< 2))
-                    
+                    cloud.scale = SIMD3(repeating: Float.random(in: 0.2 ..< 1))
+                    cloud.components[OpacityComponent.self] = .init(opacity: 0.03)
                     textChildren.addChild(cloud)
                 }
                 pointAverage = pointAverage/Float(thinnedPoints.count)
                 textChildren.position += pointAverage
                 textChildren.transform.rotation = .init(angle: .pi, axis: .init(0.0, 1.0, 0.0))
-//                positions.forEach { position in
-//                    let val = i
-//                    let cubeMaterial = UnlitMaterial(color: UIColor(red: val, green: val, blue: val, alpha: 1))
-//                    let thingy = ModelEntity(mesh: cubeMeshResource, materials: [cubeMaterial])
-//                    thingy.position = position
-//                    textEntity.addChild(thingy)
-//                    i += 1.0/CGFloat(positions.count)
-//                }
             }
             debugPrint(model.parts.count)
         }
         
         
-          return textEntity
-      }
+        return textEntity
+    }
 }
 
 #Preview (immersionStyle: .mixed) {
